b4ef261975447176640558f87588ea238664dd61
David Nachman <david@nachman.org>
2017-05-09 21:37:51 -0400

    use state to handle async

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,21 +3,30 @@ import { View, Text } from 'react-native';
 
 
 class AlbumList extends Component {
+
+  state = { albums: [] };
   
   componentWillMount() {
     console.log('componentWillMount in AlbumList');
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => response.json()
         .then(responseJson => {
-          console.log(responseJson);
+          this.setState({ albums: responseJson });
         })
       );
   }
 
+  renderAlbums() {
+    return this.state.albums.map(album => 
+      <Text key={album.title}>{album.title}</Text>
+    );
+  }
+
   render() {
+    console.log(this.state.albums);
     return (
       <View>
-        <Text>Album list</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
